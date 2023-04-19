0c7c92bd7b02b3d393591e7ad53b5061dd7c6db6
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-04-19 16:16:39 +0200

    03_Adding props

--- 03_React Basic's/src/components/ExpenseItem.js
+++ 03_React Basic's/src/components/ExpenseItem.js
@@ -1,20 +1,17 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
+const ExpenseItem = (props) => {
 	// const expenseDate = '2023.03.13';
-	const expenseDate = new Date(99, 3, 28);
 	const newDate = new Date(2002, 11, 22);
-	const expensiveTitle = 'Car Insurance';
-	const expenseAmount = 211.94;
 
 	console.log(newDate);
 
 	return (
 		<div className='expense-item'>
-			<div>{expenseDate.toISOString()}</div>
+			<div>{props.date.toISOString()}</div>
 			<div className='expense-item__description'>
-				<h2>{expensiveTitle}</h2>
-				<div className='expense-item__price'>${expenseAmount}</div>
+				<h2>{props.title}</h2>
+				<div className='expense-item__price'>${props.amount}</div>
 			</div>
 		</div>
 	);
