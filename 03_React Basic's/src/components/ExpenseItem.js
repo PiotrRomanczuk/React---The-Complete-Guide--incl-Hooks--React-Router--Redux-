29de1a221fbb86952445eac26fbc31df3a3f3724
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-04-19 17:32:49 +0200

    Splitting for Ated an Item component

--- 03_React Basic's/src/components/ExpenseItem.js
+++ 03_React Basic's/src/components/ExpenseItem.js
@@ -1,14 +1,15 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
 	// const expenseDate = '2023.03.13';
-	const newDate = new Date(2002, 11, 22);
+	// const newDate = new Date(2002, 11, 22);
 
-	console.log(newDate);
+	// console.log(newDate);
 
 	return (
 		<div className='expense-item'>
-			<div>{props.date.toISOString()}</div>
+			<ExpenseDate date={props.date} />
 			<div className='expense-item__description'>
 				<h2>{props.title}</h2>
 				<div className='expense-item__price'>${props.amount}</div>
