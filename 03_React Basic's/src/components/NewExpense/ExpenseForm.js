7ff6990793d150039ad8f0fd4d81e9cc744dba68
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-04-20 14:27:17 +0200

    Switching from 3 states to only one

--- 03_React Basic's/src/components/NewExpense/ExpenseForm.js
+++ 03_React Basic's/src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+	// const [enteredTitle, setEnteredTitle] = useState('');
+	// const [enteredAmount, setEnteredAmount] = useState('');
+	// const [enteredDate, setEnteredDate] = useState('');
+
+	const [userInput, setUserInput] = useState({
+		enteredTitle: '',
+		enteredAmount: '',
+		enteredDate: '',
+	});
+
+	const titleChangeHandler = (e) => {
+		// setEnteredTitle(e.target.value);
+		setUserInput({
+			// enteredTitle: e.target.value,
+			// enteredAmount: '',
+			// enteredDate: '',
+
+			...userInput,
+			enteredTitle: e.target.value,
+		});
+	};
+	const amountChangeHandler = (e) => {
+		// setEnteredAmount(e.target.value);
+		setUserInput({
+			// enteredTitle: '',
+			// enteredAmount: e.target.value,
+			// enteredDate: '',
+
+			...userInput,
+			enteredAmount: e.target.value,
+		});
+	};
+	const dateChangeHandler = (e) => {
+		// setEnteredDate(e.target.value);
+		setUserInput({
+			// enteredTitle: '',
+			// enteredAmount: '',
+			// enteredDate: e.target.value,
+
+			...userInput,
+			enteredDate: e.target.value,
+		});
+	};
+
 	return (
 		<form>
 			<div className='new-expense__controls'>
 				<div className='new-expense__control'>
 					<label>Title</label>
-					<input type='text' />
+					<input type='text' onChange={userInput} />
 				</div>
 				<div className='new-expense__control'>
 					<label>Amount</label>
-					<input type='number' min='0.01' step='0.01' />
+					<input type='number' min='0.01' step='0.01' onChange={userInput} />
 				</div>
 				<div className='new-expense__control'>
 					<label>Date</label>
-					<input type='date' min='2022-01-01' max='2025-12-31' />
+					<input
+						type='date'
+						min='2022-01-01'
+						max='2025-12-31'
+						onChange={userInput}
+					/>
 				</div>
 			</div>
 			<div className='new-expense__actions'>
