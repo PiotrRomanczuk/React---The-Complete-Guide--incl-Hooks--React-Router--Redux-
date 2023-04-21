9f056fa5c3e3b4869be727b4c291ac0e7495cef7
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-04-21 12:05:19 +0200

    Adding two-ways binding

--- 03_React Basic's/src/components/NewExpense/ExpenseForm.js
+++ 03_React Basic's/src/components/NewExpense/ExpenseForm.js
@@ -2,60 +2,77 @@ import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-	// const [enteredTitle, setEnteredTitle] = useState('');
-	// const [enteredAmount, setEnteredAmount] = useState('');
-	// const [enteredDate, setEnteredDate] = useState('');
+	const [enteredTitle, setEnteredTitle] = useState('');
+	const [enteredAmount, setEnteredAmount] = useState('');
+	const [enteredDate, setEnteredDate] = useState('');
 
-	const [userInput, setUserInput] = useState({
-		enteredTitle: '',
-		enteredAmount: '',
-		enteredDate: '',
-	});
+	// --- Using one "useState", for all the states ---
+	// const [userInput, setUserInput] = useState({
+	// 	enteredTitle: '',
+	// 	enteredAmount: '',
+	// 	enteredDate: '',
+	// });
 
 	const titleChangeHandler = (e) => {
-		// setEnteredTitle(e.target.value);
-		setUserInput({
-			// enteredTitle: e.target.value,
-			// enteredAmount: '',
-			// enteredDate: '',
-
-			...userInput,
-			enteredTitle: e.target.value,
-		});
+		setEnteredTitle(e.target.value);
+		//
+		// setUserInput({
+		// ---  This one is correct, but it could be spread other way ---
+		// enteredTitle: e.target.value,
+		// enteredAmount: '',
+		// enteredDate: '',
+		//
+		// --- Other way of spreading the inputs ---
+		// 	...userInput,
+		// 	enteredTitle: e.target.value,
+		// });
+		//
+		// --- Picking up the prevState to the handler ---
+		// setUserInput((prevState) => {
+		// 	return { ...prevState, enteredTitle: e.target.value };
 	};
 	const amountChangeHandler = (e) => {
-		// setEnteredAmount(e.target.value);
-		setUserInput({
-			// enteredTitle: '',
-			// enteredAmount: e.target.value,
-			// enteredDate: '',
-
-			...userInput,
-			enteredAmount: e.target.value,
-		});
+		setEnteredAmount(e.target.value);
 	};
 	const dateChangeHandler = (e) => {
-		// setEnteredDate(e.target.value);
-		setUserInput({
-			// enteredTitle: '',
-			// enteredAmount: '',
-			// enteredDate: e.target.value,
+		setEnteredDate(e.target.value);
+	};
 
-			...userInput,
-			enteredDate: e.target.value,
-		});
+	const submitHandler = (e) => {
+		e.preventDefault();
+
+		const expenseData = {
+			title: enteredTitle,
+			amount: enteredAmount,
+			date: new Date(enteredDate),
+		};
+
+		console.log(expenseData);
+		setEnteredTitle('');
+		setEnteredAmount('');
+		setEnteredDate('');
 	};
 
 	return (
-		<form>
+		<form onSubmit={submitHandler}>
 			<div className='new-expense__controls'>
 				<div className='new-expense__control'>
 					<label>Title</label>
-					<input type='text' onChange={userInput} />
+					<input
+						type='text'
+						value={enteredTitle}
+						onChange={titleChangeHandler}
+					/>
 				</div>
 				<div className='new-expense__control'>
 					<label>Amount</label>
-					<input type='number' min='0.01' step='0.01' onChange={userInput} />
+					<input
+						type='number'
+						min='0.01'
+						step='0.01'
+						value={enteredAmount}
+						onChange={amountChangeHandler}
+					/>
 				</div>
 				<div className='new-expense__control'>
 					<label>Date</label>
@@ -63,7 +80,8 @@ const ExpenseForm = () => {
 						type='date'
 						min='2022-01-01'
 						max='2025-12-31'
-						onChange={userInput}
+						value={enteredDate}
+						onChange={dateChangeHandler}
 					/>
 				</div>
 			</div>
