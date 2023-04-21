e151c0bd26d8c1d909845ed8f436acbac91be3e6
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-04-21 16:30:32 +0200

    Adding conditionals filter logic to the Expenses component

--- 03_React Basic's/src/components/Expenses/Expenses.js
+++ 03_React Basic's/src/components/Expenses/Expenses.js
@@ -16,20 +16,26 @@ function Expenses(props) {
 		return expense.date.getFullYear().toString() === filteredYear;
 	});
 
+	let expenseContent = <p>No content found</p>;
+
+	if (filteredExpenses.length > 0) {
+		expenseContent = filteredExpenses.map((expense, idx) => (
+			<ExpenseItem
+				key={expense.id}
+				title={expense.title}
+				amount={expense.amount}
+				date={expense.date}
+			/>
+		));
+	}
+
 	return (
 		<Card className='expenses'>
 			<ExpensesFilter
 				selected={filteredYear}
 				onChangeFilter={filterChangeHandler}
 			/>
-			{filteredExpenses.map((expense, idx) => (
-				<ExpenseItem
-					key={expense.id}
-					title={expense.title}
-					amount={expense.amount}
-					date={expense.date}
-				/>
-			))}
+			{expenseContent}
 		</Card>
 	);
 }
