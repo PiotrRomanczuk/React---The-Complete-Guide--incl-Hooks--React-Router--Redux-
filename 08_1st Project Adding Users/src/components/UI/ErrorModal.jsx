043119c6b128a434f22bbcf973457e3bd4e7bc48
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-04-25 14:57:53 +0200

    .

--- 08_1st Project Adding Users/src/components/UI/ErrorModal.jsx
+++ 08_1st Project Adding Users/src/components/UI/ErrorModal.jsx
@@ -5,19 +5,22 @@ import classes from './ErrorModal.module.css';
 
 const ErrorModal = (props) => {
 	return (
-		<div className={classes.backdrop} onClick={props.onConfirm}>
-			<Card className={classes.modal}>
-				<header className={classes.header}>
-					<h2>{props.title}</h2>
-				</header>
-				<div className={classes.content}>
-					<p>{props.message}</p>
-				</div>
-				<footer className={classes.actions}>
-					<Button onClick={props.onConfirm}>Ok</Button>
-				</footer>
-			</Card>
-		</div>
+		<React.Fragment>
+			<div className={classes.backdrop} onClick={props.onConfirm}>
+				<Card className={classes.modal}>
+					<header className={classes.header}>
+						<h2>{props.title}</h2>
+					</header>
+					<div className={classes.content}>
+						<p>{props.message}</p>
+					</div>
+					<footer className={classes.actions}>
+						<Button onClick={props.onConfirm}>Ok</Button>
+					</footer>
+					8
+				</Card>
+			</div>
+		</React.Fragment>
 	);
 };
 
