885e6c708a44ea22690b414a725958e799094166
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-04-29 18:54:36 +0200

    Adding HeaderCartButton

--- 11_Food Order App/src/components/Layout/Header.js
+++ 11_Food Order App/src/components/Layout/Header.js
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
 import classes from './Header.module.css';
 import mealsImage from '../../assets/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = (props) => {
 	return (
@@ -8,7 +9,7 @@ const Header = (props) => {
 			<header className={classes.header}>
 				<h1>React Meals</h1>
 
-				<button>Cart</button>
+				<HeaderCartButton />
 			</header>
 			<div className={classes['main-image']}>
 				<img src={mealsImage} alt='meals'></img>
