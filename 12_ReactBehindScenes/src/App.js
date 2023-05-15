bd837a2a17af7444014cd0469846fda9ea1446e1
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-05-15 16:40:52 +0200

    Adding 14 section

--- 12_ReactBehindScenes/src/App.js
+++ 12_ReactBehindScenes/src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
+import Button from './components/UI/Button/Button';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
-  return (
-    <div className="app">
-      <h1>Hi there!</h1>
-    </div>
-  );
+	const [showParagraph, setShowParagraph] = useState(false);
+
+	const togglePragraphHandler = () => {
+		setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+	};
+
+	console.log('App works');
+
+	return (
+		<div className='app'>
+			<h1>Hi there!</h1>
+			<DemoOutput show={showParagraph} />
+			<Button onClick={togglePragraphHandler}>This is the button</Button>
+		</div>
+	);
 }
 
 export default App;
