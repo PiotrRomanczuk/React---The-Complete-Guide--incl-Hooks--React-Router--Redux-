328cf820fbd7ac21eeab80c1c62bfc18c4c56711
K0st3k696 <77811265+PiotrRomanczuk@users.noreply.github.com>
2023-05-15 22:24:30 +0200

    Fetching the data from API & basic rendering it

--- 14_HTTP/src/App.js
+++ 14_HTTP/src/App.js
@@ -1,34 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+	const BASE_URL = 'https://swapi.py4e.com/';
+	const [movies, setMovies] = useState([]);
 
-  return (
-    <React.Fragment>
-      <section>
-        <button>Fetch Movies</button>
-      </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
-    </React.Fragment>
-  );
+	const fetchMoviesHandler = () => {
+		fetch(BASE_URL + '/api/films')
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => setMovies(data.results));
+		// .catch((err) => console.error(err));
+	};
+
+	fetchMoviesHandler();
+
+	return (
+		<React.Fragment>
+			<section>
+				<button>Fetch Movies</button>
+			</section>
+			<section>
+				<MoviesList movies={movies} />
+			</section>
+		</React.Fragment>
+	);
 }
 
 export default App;
